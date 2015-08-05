ea3209dcff54ee5c2a0ff6886bf800b90a1c6e6d
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-05 07:02:44 -0700

    Chapter 8: More Dicts

---
+++ Ch08_04MoreDict.py
@@ -0,0 +1,16 @@
+print("Dictionaries to simulate flexible lists")
+L=[]
+#L[99]='spam' # will have a failure
+
+D={}
+D[99]='spam'
+print(D)
+
+#Note integers are used as strings
+table={1975:"Holy Grail",
+		1979:"Life of Brian",
+		1983:"The meaning of Life"}
+
+print(table)
+
+print("\n#Using dictionaries for sparse data structures: Tuple keys")
