5846e32a6e07f0d1d3ee48bcd3b6f09447d16a2a
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-08 06:57:21 -0700

    minor updates ch28

--- Ch28_01AdvClass.py
+++ Ch28_01AdvClass.py
@@ -4,6 +4,11 @@ class Person:
 		self.job=job
 		self.pay=pay
 
+	def lastname(self):
+		return self.name.split()[-1]
+
+	def giveRaise(self,percent):
+		self.pay=int(self.pay+percent*self.pay)
 
 if __name__=='__main__':
 	#self-test code
@@ -11,3 +16,6 @@ if __name__=='__main__':
 	sue = Person('Sue Jones', job='dev', pay=100000)
 	print(bob.name, bob.pay)
 	print(sue.name, sue.pay)
+	print("Bob's lname:", bob.lastname(), "Sue's lname:",sue.lastname())
+	sue.giveRaise(0.1)
+	print("Sue's pay after raise:", sue.pay)
