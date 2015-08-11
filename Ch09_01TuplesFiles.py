119691fd9b38b47ce6c227e56ebb690325b9f904
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-11 07:30:28 -0700

    Ch 09 Tuples and Files started

---
+++ Ch09_01TuplesFiles.py
@@ -0,0 +1,10 @@
+#Tuple are like dictionaries but not mutable
+
+print("Tuple Concat (1,2)+(3,4)=", (1,2)+(3,4))
+print("Tuple Repeat (1,2)*4=", (1,2)*4)
+T=(1,2,3,4)
+print("Tuple T=",T)
+print("Indexing T[0]=", T[0])
+print("Indexing T[:3]=",T[0:3])
+
+T=('cc','aa','dd','bb')
