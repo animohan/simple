24aaa9fad2b3870d8bd7e171f7859b6b12bba85b
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-11 07:19:19 -0700

    minor updates ch28

--- Ch28_01AdvClass.py
+++ Ch28_01AdvClass.py
@@ -4,7 +4,7 @@ class Person:
 		self.job=job
 		self.pay=pay
 
-	def lastname(self):
+	def lastName(self):
 		return self.name.split()[-1]
 
 	def giveRaise(self,percent):
@@ -16,9 +16,28 @@ class Person:
 
 #defining subclasses
 class Manager(Person):
+	def __init__(self, name, pay):
+		Person.__init__(self,name,'mgr',pay)
+
 	def giveRaise(self, percent, bonus=0.10):
 		Person.giveRaise(self, percent+bonus)
 
+class Department:
+	def __init__(self,*args):
+		self.members=list(args)
+
+
+	def addMember(self, person):
+		self.members.append(person)
+
+
+	def showAll(self):
+		for person in self.members:
+			print(person)
+
+	def giveRaises(self, percent):
+		for person in self.members:
+			person.giveRaise(percent)
 
 if __name__=='__main__':
 	#self-test code
@@ -26,7 +45,22 @@ if __name__=='__main__':
 	sue = Person('Sue Jones', job='dev', pay=100000)
 	print(bob.name, bob.pay)
 	print(sue.name, sue.pay)
-	print("Bob's lname:", bob.lastname(), "Sue's lname:",sue.lastname())
+	print("Bob's lname:", bob.lastName(), "Sue's lname:",sue.lastName())
 	sue.giveRaise(0.1)
 	print("Sue's pay after raise:", sue.pay)
 	print(sue)
+	tom = Manager ('Tom Jones',50000)
+	tom.giveRaise(0.10) #Runs the manager version of giveRaise
+	print(tom.lastName()) #runs inherited method
+	print(tom)
+
+	print('---All Three---')
+	for obj in (bob,sue,tom):
+		obj.giveRaise(.10)
+		print(obj)
+
+
+	development=Department(bob,sue)
+	development.addMember(tom)
+	development.giveRaises(0.10)
+	development.showAll()
