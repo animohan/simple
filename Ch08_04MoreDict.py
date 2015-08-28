7bcfcd740ab87fb7ac48dc906c1674c8ed7708db
animohan <anishmohan@gmail.com>
2015-08-27 20:57:58 -0700

    Minor edits to dictionary

--- Ch08_04MoreDict.py
+++ Ch08_04MoreDict.py
@@ -20,3 +20,19 @@ Matrix[(7,8,9)]=99
 X=2;Y=3;Z=4
 print(Matrix[(X,Y,Z)])
 print(Matrix)
+
+#if matrix is accessed with non existent key, it will throw an error; Here is a fix
+
+if(2,3,6) in Matrix:
+    print(Matrix[(2,3,6)])
+else:
+    print(0)
+
+#other option
+try:
+    print(Matrix[(2,3,4)])
+except KeyError:
+    print(0)
+
+print(Matrix.get((2,3,4),0))
+print(Matrix.get((2,3,6),0))
